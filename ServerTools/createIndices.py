110d291e88d44c0d1db9226055dbfca41df62674
Hannu <hannu.j.viitala@gmail.com>
2022-01-30 16:13:32 +0200

    Added timestamps

--- ServerTools/createIndices.py
+++ ServerTools/createIndices.py
@@ -1,17 +1,21 @@
 # A script for generating index.json files
 
 import os
+import datetime
+import struct
+import time
+
 
 defaultFolderList = [
-    "0Action",
-    "1Adventure",
-    "2Arcade",
-    "3Platformer",
-    "4Puzzle",
-    "5Racing",
-    "6Strategy",
-    "7Demos",
-    "8Utils",
+    ["0Action", "Action"],
+    ["1Adventure", "Adventure"],
+    ["2Arcade", "Arcade"],
+    ["3Platformer", "Platformer"],
+    ["4Puzzle", "Puzzle"],
+    ["5Racing", "Racing"],
+    ["6Strategy", "Strategy"],
+    ["7Demos", "Demos"],
+    ["8Utils", "Utils"],
 ]
 
 defaultFolderList_test = [
@@ -34,19 +38,25 @@ def CreateRootIndex():
     # write the folders
     for i in range(len(defaultFolderList)):
 
-        folderName = defaultFolderList[i]
+        folderName = defaultFolderList[i][0]
+        prettyName = defaultFolderList[i][1]
+        timestamp = defaultFolderList[i][2]
+
+
 
         # Write a folder record, e.g.
- 	    #{ 
-		#   "type": "dir", 
-		#   "dirtitle": "0Action", 
-		#   "file": "0Action"
-	    #},
+        #{ 
+        #   "type": "dir", 
+        #   "dirtitle": "0Action", 
+        #   "file": "0Action"
+        #},
  
         file.write("   {\n")
         file.write('      "type": "dir",\n')
-        file.write('      "dirtitle": "' + folderName + '",\n')
+        file.write('      "dirtitle": "' + prettyName + '",\n')
         file.write('      "file": "' + folderName + '"\n')
+        file.write('      "timestamp": "' + timestamp + '"\n')
+
         if i < len(defaultFolderList) - 1:
             file.write("   },\n") # Add ','
         else:
@@ -66,15 +76,18 @@ def ReadTagFromPopFile(popFile):
     file=open(popFile, "rb")
     tagBytes = file.read(4)
     while(tagBytes):
-        tag = int.from_bytes(tagBytes, 'little')
+        tag = struct.unpack("<i",tagBytes)[0]
+        #tag = int.from_bytes(tagBytes, 'little')
         print("tag=", tag)
         lenghtBytes = file.read(4)
-        lenght = int.from_bytes(lenghtBytes, 'little')
+        lenght = struct.unpack("<i",lenghtBytes)[0]
+        #lenght = int.from_bytes(lenghtBytes, 'little')
         print("lenght=", lenght)
         if tag == nameTagId: 
             # read name
             name = file.read(lenght-1) # exclude the ending null
-            return str(name, 'utf-8')
+            return name.encode('utf-8')
+			#return str(name, 'utf-8')
 
         # skip tag            
         file.read(lenght)
@@ -85,21 +98,40 @@ def ReadTagFromPopFile(popFile):
 
 def CreateSubdirIndices():
 
-    for dir in defaultFolderList:
+    for folderListIndex in range(len(defaultFolderList)):
+        folderListItem = defaultFolderList[folderListIndex]
+        dir = folderListItem[0]
         print(dir)
         fileListAll = os.listdir("../"+dir)
         print(fileListAll)
 
         # Drop other files than *.bin and *.pop
         fileList = []
+        maxTimeStr = ""
+        maxTimeSinceEpochInSecInt = 0
         for item in fileListAll:
             nameAndExtList = item.split('.')
             ext = nameAndExtList[1]
             if ext == "pop" or ext == "bin":
-                fileList.append(item)
+                # Read the timestamp
+            	filePath1 = "../"+dir+"/"+item
+                timeSinceEpochInSeconds = os.path.getmtime(filePath1) 
+                timeSinceEpochInSecInt = int(timeSinceEpochInSeconds)
+                timeSinceEpochInSecStr = str(timeSinceEpochInSecInt)
+                #print('timeSinceEpochInSecStr',timeSinceEpochInSecStr)
+
+                # Store the newest timestamp
+                if timeSinceEpochInSecInt > maxTimeSinceEpochInSecInt:
+                    maxTimeSinceEpochInSecInt = timeSinceEpochInSecInt
+                    maxTimeStr = timeSinceEpochInSecStr
+
+                fileList.append([item, timeSinceEpochInSecStr])
+
+        # store the timestamp for the folder
+		defaultFolderList[folderListIndex] = [folderListItem[0], folderListItem[1], maxTimeStr]
 
         # Sort alphapetically
-        fileList.sort()
+        fileList.sort(key=lambda tmp: tmp[0])
 
         file = open("../"+dir+"/index.json", "w")
 
@@ -110,6 +142,7 @@ def CreateSubdirIndices():
 
         file.write("{\n")
         file.write('"path":"https://raw.githubusercontent.com/haviital/GameDisk2/master/' + dir +'"\n')
+        file.write('"timestamp":"' + maxTimeStr + '"\n')
         file.write('"list": [\n')
     
 
@@ -117,27 +150,44 @@ def CreateSubdirIndices():
         # write games
         for i in range(len(fileList)):
 
-            gameFile = fileList[i]
+            gameFile = fileList[i][0]
             nameAndExtList = gameFile.split('.')
             name = nameAndExtList[0]
             ext = nameAndExtList[1]
 
             # Read the long name from pop
+            filePath2 = "../"+dir+"/"+gameFile
             if ext == "pop":
-                name = ReadTagFromPopFile("../"+dir+"/"+gameFile)
+                name = ReadTagFromPopFile(filePath2)
+                print("pop name=" +  name)
+
+            # Read the timestamp
+            timeSinceEpochInSecStr = fileList[i][1]
+            
+			#timeSinceEpochInSeconds = os.path.getmtime(filePath2) 
+            #timeSinceEpochInSecInt = int(timeSinceEpochInSeconds)
+            #timeSinceEpochInSecStr = str(timeSinceEpochInSecInt)
+            #print('timeSinceEpochInSecStr',timeSinceEpochInSecStr)
+
+            #struct_timeUTC = time.gmtime(timeSinceEpochInSeconds)
+            #dt = datetime.fromtimestamp(mktime(struct_timeUTC))
+            #timeISO8601UTC = dt.replace(microsecond=0).isoformat()
 
             # Write a game record, e.g.
             #{ 
             #   "type": "pop", 
             #   "title": "Ball Bust", 
             #   "file": "B-Bust.pop",
-            #   "shots": "B-Bust.bmp"
+            #   "shots": "B-Bust.bmp", # not yet used
+            #   "timestamp": "", 
             #},
 
             file.write("   {\n")
             file.write('      "type": "' + ext + '",\n')
             file.write('      "title": "' + name + '",\n')
-            file.write('      "file": "' + gameFile + '"\n')
+            file.write('      "file": "' + gameFile + '",\n')
+            file.write('      "timestamp": "' + timeSinceEpochInSecStr + '"\n')
+
             if i < len(fileList) - 1:
                 file.write("   },\n") # Add ','
             else:
